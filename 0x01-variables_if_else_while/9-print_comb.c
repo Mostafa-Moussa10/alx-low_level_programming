7e5a845d1c20f31694837fd378a3d737bfb63054
mostafa <mostafabadry95@gmail.com>
2023-06-16 12:41:55 -0700

    auto2

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+
+int main(void)
+{
+
+	int x = 0;
+
+	while (x < 10)
+	{
+		putchar('0' + x);
+		if (x != 9)
+		{
+			putchar(',');
+		}
+		else 
+		{
+			putchar('$');
+		}
+
+		putchar(' ');
+		x++;
+
+	}
+
+	return (0);
+}
