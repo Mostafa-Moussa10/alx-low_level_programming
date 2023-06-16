b97855ee97a6a7569c04f7510a0144efb5501d51
mostafa <mostafabadry95@gmail.com>
2023-06-16 11:55:50 -0700

    auto

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/*
+ * main - fuction to print numbers as character
+ *
+ * @ch: variable to carry the value of number
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int ch;
+
+	for (ch = '0' ; ch < '10' ; ch++)
+	{
+		putchar('ch');
+	}
+
+	/*new line*/
+
+	putchar('\n');
+
+	return (0);
+}
