a97f2f0d7c3e320e623426ba366c815aac7a8cbd
mostafa <mostafabadry95@gmail.com>
2023-06-16 13:22:26 -0700

    auto2

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,12 +9,11 @@ int main(void)
 
 	while (x < 10)
 	{
-			
 		while (y  < 10)	
 		{
 			putchar('0' + x);
 			putchar('0' + y);
-			if ((x != 9) && (y != 8))
+			if ((x != 8) && (y != 9))
 			{
 				putchar(',');
 			}
