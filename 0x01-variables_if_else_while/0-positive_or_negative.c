87e8178a23f4292db82cb3cf1189141ded210bf6
mostafa <mostafabadry95@gmail.com>
2023-06-16 06:58:25 -0700

    auto

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,10 +13,11 @@
  * Return: 0
  */
 
-int main(time(0))
+int main(void)
 {
 	int n;
 
+	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
 	if (n > 0)
@@ -31,5 +32,7 @@ int main(time(0))
 	{
 		printf("%d is negative\n", &n);
 	}
+
+	return (0);
 }
 
