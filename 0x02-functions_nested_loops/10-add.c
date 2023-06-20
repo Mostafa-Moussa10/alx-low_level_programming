5aa138bc7b5547cb9d4e5860275b533c72a0cfa9
mostafa <mostafabadry95@gmail.com>
2023-06-20 13:57:20 -0700

    auto

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - function to get the sum of two numbers
+ *
+ * @a: the first number
+ * @b: the secomd  number
+ *
+ * Return: sum of the two numbers
+ */
+
+int add(int a, int b)
+{
+	int s = a + b;
+	return (s);
+}
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -16,40 +16,27 @@ void times_table(void)
 		j = 1;
 		_putchar('0');
 		_putchar(',');
-
 		while (j < 10)
 		{
 			k = i * j;
-
 			_putchar(' ');
-			
-
-
 			if (k < 10)
 			{
 				_putchar(' ');
 				_putchar('0' + k);
-			
 			}
-			
 			else
 			{
 				_putchar('0' + k / 10);
 				_putchar('0' + k % 10);
 			}
-			  
-			if (j != 9)      
+			if (j != 9)
 			{
-                                   
-			   	_putchar(',');
-                        
+				_putchar(',');
 			}
 			j++;
-
 		}
-
 		_putchar('\n');
-
 		i++;
 	}
 }
