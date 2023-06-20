e32770163bfdcfad01fdd845cddd38fd888a4e4a
mostafa <mostafabadry95@gmail.com>
2023-06-20 11:52:26 -0700

    auto

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _abs - function to get the absolute value of number
+ *
+ * @n: the variable  to carry the input
+ *
+ * Return: the absolute value
+ */
+
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (-n);
+	}
+}
