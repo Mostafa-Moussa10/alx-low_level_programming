69cb8fec8f95c647a27f7a12a6fad31b10ba6d01
mostafa <mostafabadry95@gmail.com>
2023-06-20 12:48:30 -0700

    auto

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * times_table - function to print the times table up to 9
+ */
+
+void times_table(void)
+{
+	int i = 0;
+	int j = 0;
+
+
+	while (i < 10)
+	{
+		j = 0;
+		while (j < 10)
+		{
+			_putchar('0' + i * j);
+			if (j != 9)
+			{
+				_putchar(',');
+			}
+			_putchar(' ');
+			j++;
+		}
+		_putchar('\n');
+		i++
+	}
+}
