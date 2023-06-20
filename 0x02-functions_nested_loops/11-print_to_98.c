e7138733618b3bd56db4b6b005fe08ff09b33439
mostafa <mostafabadry95@gmail.com>
2023-06-20 14:19:05 -0700

    auto

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,52 @@
+#include "main.h"
+
+/**
+ * print_to_98 - function to print to 98
+ *
+ * @n: start from
+ */
+
+void print_to_98(int n)
+{
+	if (n < 98)
+	{
+		while (n < 98)
+		{
+			if (n < 0)
+			{
+				n = -n;
+				_putchar('-');
+			}
+			print_digit(n);
+			_putchar(',');
+			_putchar(' ');
+			n++;
+		}
+	}
+	else
+	{
+		while (n > 98)
+		{
+			print_digit(n);
+			_putchar(',');
+			_putchar(' ');
+			n--;
+		}
+
+	}
+	print_digit(98);
+
+	
+}
+void print_digit(int n)
+{
+	if (n < 10)
+	{
+		_putchar('0' + n);
+	}
+	else
+	{
+		_putchar('0' + n / 10);
+		_putchar('0' + n % 10);
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -16,7 +16,7 @@ void jack_bauer(void);
 void times_table(void);
 int add(int a, int b);
 void print_to_98(int n);
-
+void print_digit(int n);
 
 
 #endif
