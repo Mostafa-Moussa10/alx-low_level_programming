830668d4fe8cb3364bdf5144f47ced823afaff9d
mostafa <mostafabadry95@gmail.com>
2023-06-22 06:41:56 -0700

    auto

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_square - print a diagonal line
+ * @n: length of the square
+ */
+
+void print_diagonal(int size)
+{
+	int i = 0;
+	int j = 0;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < size; i++)
+		{
+			j = 0;
+			while (j < size)
+			{
+				_putchar('#');
+				j++;
+			}
+			_putchar('\n');
+		}
+	}
+}
