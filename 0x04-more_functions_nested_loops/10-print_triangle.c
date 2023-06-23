68bcd94034714871b6129df1959674daee4b837e
mostafa <mostafabadry95@gmail.com>
2023-06-23 06:25:58 -0700

    auto

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,39 @@
+#include "main.c"
+
+/**
+ * print_triangle - function to  print triangle of size n
+ *
+ * @size: size of triangle
+ */
+
+void print_triangle(int size)
+{
+	int i = 0; /*counter*/
+	int j = 1; /*number of line*/
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		while (j <= size)
+		{
+			i = 0;
+			while (i < size - j)
+			{
+				_putchar(' ');
+				i++;
+			}
+			while (i < size)
+			{
+				_putchar('#');
+				i++;
+			}
+			j++;
+			_putchar('\n');
+		}
+	}
+}
+
+
