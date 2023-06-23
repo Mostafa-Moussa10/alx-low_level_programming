52dd9dded4fbfd5099d0f3c958ada2671ab10316
mostafa <mostafabadry95@gmail.com>
2023-06-23 05:32:56 -0700

    auto

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,46 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - functon to print fizz buzz 
+ *
+ * Return: 1 if success
+ */
+
+int main(void)
+{
+	fizz_buzz();
+
+	return(1);
+}
+
+/**
+ * fizz_buzz - function to print fizz buzz sendrom
+ */
+
+void fizz_buzz(void)
+{
+	int n = 0;
+
+	while (n <= 100)
+	{
+		if (n % 3 == 0 && n % 5 != 0)
+		{
+			printf("Fizz");
+		}
+		else if (n % 3 != 0 && n % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else if(n % 3 == 0 && n % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else
+		{
+			printf("%d");
+		}
+		printf(" ");
+		n++;
+	}
+}
