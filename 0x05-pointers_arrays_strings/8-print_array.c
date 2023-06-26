105fb76259816ffac4b6c01c4d0f357ae3076e35
mostafa <mostafabadry95@gmail.com>
2023-06-26 11:09:29 -0700

    automatic

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,3 +1,21 @@
 #include <stdio.h>
 
 /**
+ * print_array - function to print specific element of array of integers
+ *
+ * @a: pointer to array
+ * @n: number of element
+ */
+
+void print_array(int *a, int n)
+{
+	int i = 0; /*counter*/
+	while (i < n - 1)
+	{
+		printf("%d, ", a[i]);
+		i++;
+	}
+	printf("%d\n", a[i]);
+
+}
+
