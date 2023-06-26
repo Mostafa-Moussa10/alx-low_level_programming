e551b1e2eb20411464916c525d26b379e4d54aa3
mostafa <mostafabadry95@gmail.com>
2023-06-26 12:32:21 -0700

    automatic

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -10,12 +10,20 @@
 void print_array(int *a, int n)
 {
 	int i = 0; /*counter*/
-	while (i < n - 1)
+	
+	if (n < 0)
 	{
-		printf("%d, ", a[i]);
-		i++;
 	}
-	printf("%d\n", a[i]);
+	else
+	{
+		while (i < n - 1)
+		{
+			printf("%d, ", a[i]);
+			i++;
+		}
 
+	
+		printf("%d\n", a[i]);
+	}
 }
 
