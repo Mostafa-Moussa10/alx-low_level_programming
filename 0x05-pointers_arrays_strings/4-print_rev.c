ea47543c78764f05b2894997c6bfa7a35c9257e5
mostafa <mostafabadry95@gmail.com>
2023-06-26 06:15:44 -0700

    automatic

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -11,13 +11,12 @@ void print_rev(char *s)
 {
 	int len = strlen(s);
 	int i = 0; /*counter*/
-	
+
 	while (i < len)
 	{
 		printf("%c",  s[len - i - 1]);
-
 		i++;
-	}	
+	}
 	printf("\n");
 }
 
---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * rev_string - function to reverse a string
+ *
+ * @s: pointer to the string
+ */
+
+void rev_string(char *s)
+{
+	int i = 0; /*counter*/
+	char str = 0;
+	int len = strlen(s);
+
+	while (i < len)
+	{
+	       str = s[i];
+	       s[i] = s[len - i - 1];
+	       s[len - i - 1] = str;
+	}
+}
