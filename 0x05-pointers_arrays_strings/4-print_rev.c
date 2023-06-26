b0dc3cfcfcee276a56b0ac97e9a4dcdbf135dfa9
mostafa <mostafabadry95@gmail.com>
2023-06-26 06:04:21 -0700

    automatic

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -11,11 +11,12 @@ void print_rev(char *s)
 {
 	int len = strlen(s);
 	int i = 0; /*counter*/
-	char *str;
+	char *str = NULL;
 
 	while (i < len)
 	{
 		str[i] = s[len - i - 1];
+
 		i++;
 	}	
 	printf("%s\n", str);
