556c0d0519d4b795d6323eddc8df3dec1292e007
mostafa <mostafabadry95@gmail.com>
2023-06-26 06:02:05 -0700

    automatic

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * print_rev - function to print the revesed string
+ *
+ * @s: pointer to the string
+ */
+
+void print_rev(char *s)
+{
+	int len = strlen(s);
+	int i = 0; /*counter*/
+	char *str;
+
+	while (i < len)
+	{
+		str[i] = s[len - i - 1]
+		i++;
+	}	
+	printf("%s\n", str);
+}
+
