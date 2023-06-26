03c36479e69a35422936380498c2e90c1e590e7b
mostafa <mostafabadry95@gmail.com>
2023-06-26 05:51:18 -0700

    automatic

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * _puts - function to print a string followed by a new line
+ *
+ * @str: pointer to the string
+ */
+
+void _puts(char *str)
+{
+	printf("%s\n", str);
+}
+
