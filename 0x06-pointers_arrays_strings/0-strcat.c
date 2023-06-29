cc2e5bcbd5656f7f449b1ff51788742cf8963a2d
mostafa <mostafabadry95@gmail.com>
2023-06-29 03:51:10 -0700

    mostfa

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -12,11 +12,26 @@
 
 char *_strcat(char *dest, char *src)
 {
+	int i = 0; /*counter*/
+	int j = 0; /*counter*/
 
-	strcat(dest, src);
+	/*length of string*/
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
 
+	/*concatenates src to dest*/
+	while (s[j] != '\0')
+	{
+		dest[i] = src[j];
+		j++;
+		i++;
+	}
 
-
+	/*add \0*/
+	dest[i] = src[j];
+	
 	return (dest);
 }
 
