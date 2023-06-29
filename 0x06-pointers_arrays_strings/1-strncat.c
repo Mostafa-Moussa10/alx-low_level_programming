dd15717b9f016609b1360d9e9e1311f066bb2853
mostafa <mostafabadry95@gmail.com>
2023-06-29 04:41:13 -0700

    mostfa

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -23,11 +23,16 @@ char *_strncat(char *dest, char *src, int n)
 		i++;
 	}
 
-	while (j < n - 1 || src[j] != '\0')
+	while (j < n || src[j] != '\0')
 	{
 		dest[i] = src[j];
 		j++;
 		i++;
+	}
+
+	while (src[j] != '\0')
+	{
+		j++;
 	}
 
 	/*copy null*/
