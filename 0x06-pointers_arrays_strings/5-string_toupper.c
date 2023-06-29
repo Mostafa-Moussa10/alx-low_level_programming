c7a227a3dbc229536366992df968b6180b1039f7
mostafa <mostafabadry95@gmail.com>
2023-06-29 07:28:30 -0700

    mostfa

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * string_toupper - function to change all letters to upppercase
+ *
+ * @s: pointer to string
+ *
+ * Return: the resultant string
+ */
+
+char *string_toupper(char *s)
+{
+	int i = 0; /*counter*/
+
+	while (s[i] < '\0')
+	{
+		s[i] = s[i] + 'A' - 'a';
+		i++;
+	}
+	return (s);
+}
+
