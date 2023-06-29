94432ceaddf6e53364b448007a7cbca3d6b77846
mostafa <mostafabadry95@gmail.com>
2023-06-29 02:50:36 -0700

    mostfa

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strcat - function to concatenates two strings
+ *
+ * @dest: first string
+ * @src: second string
+ *
+ * Return: the resulting string dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+
+
+
+
+	return (dest);
+}
+
