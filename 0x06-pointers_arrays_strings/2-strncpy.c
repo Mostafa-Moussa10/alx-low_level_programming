576318d19e2279b588e8e3d50caeee4e4a403b74
mostafa <mostafabadry95@gmail.com>
2023-06-29 05:12:39 -0700

    mostfa

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,33 @@
+#include "main."
+
+/**
+ * _strncpy = fuction to overwrite n of letters in string
+ *
+ * @dest: first string
+ * @src: second string
+ * @n: number of letters
+ *
+ * Return: the value of dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	/*counters*/
+	int i = 0;
+	int j = 0;
+
+	while (i < n && src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+
+	j = i;
+
+	if (src[i] == '\0')
+	{
+		dest[j] = src[i];
+	}
+	
+	return (dest);
+}
