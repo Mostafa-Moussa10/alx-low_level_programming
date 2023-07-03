ee85bf12ae13c3c99d1166abe3c90164ecdc5f72
mostafa <mostafabadry95@gmail.com>
2023-07-03 10:11:09 -0700

    mostafa

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _memset - function to fill the firest n bytes of the memory
+ *
+ * @s: pointer to the first byte of the memory
+ * @b: constant to fill the bytes with
+ * @n: number of bytes to fill
+ *
+ * Return: the value of s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+
+
+
+
+	return (s);
+}
+
