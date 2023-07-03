7ffd04b53df995dff2a31e2affa91431b2688688
mostafa <mostafabadry95@gmail.com>
2023-07-03 11:11:27 -0700

    mostafa

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strspn - function to get the number of intial segment
+ *
+ * @s: pointer to the main string
+ * @accept: pointer to the values to check
+ *
+ * Retyrn: number of bytes
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	/*counter*/
+	unsigned int i = 0;
+	unsigned int j = 0;
+	unsigned int num = 0;
+	unsigned int len1 = strlen(s);
+	unsigned int len2 = strlen(accept);
+
+
+	while (i < len2)
+	{
+		for (j = 0; j < len1; j++)
+		{
+			if (accept[i] == s[j])
+			{
+				num++;
+				break;
+			}
+		}
+		i++;
+	}
+	return (num);
+}
