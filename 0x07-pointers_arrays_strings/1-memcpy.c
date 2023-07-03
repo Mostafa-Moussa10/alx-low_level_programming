5412aa8bd9edd15aaabf29363f97c65a95bbb548
mostafa <mostafabadry95@gmail.com>
2023-07-03 10:29:52 -0700

    mostafa

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _memcpy - functionn to copy n bytes from a string to another
+ *
+ * @dest: pointer to the first string
+ * @src: pointer to the second string
+ * @n: the number of bytes
+ *
+ * Return: the value of the first string
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	/*counter*/
+	unsigned int i = 0;
+
+	while (i < n)
+	{
+		src[i] = dest[i];
+		i++;
+	}
+
+	return (dest);
+}
