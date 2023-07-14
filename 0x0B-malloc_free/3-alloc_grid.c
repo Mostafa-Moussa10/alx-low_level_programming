5519ab5ef180c2f795ccc076b05fd43bc09b3bbc
mostafa <mostafabadry95@gmail.com>
2023-07-14 12:55:40 -0700

    memory allocation

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -4,8 +4,8 @@
 /**
  * alloc_grid - function to declare two dimenssion array
  *
- * @width: width of array
- * @height: height of the array
+ * @width: number of elements of one array
+ * @height: number of arrays
  *
  * Return: pointer to array
  */
@@ -13,21 +13,41 @@
 int **alloc_grid(int width, int height)
 {
 	int i = 0;
+	int j = 0;
 	int **p;
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	p = (int **) malloc(sizeof(int) * width * height);
+	p = (int **) malloc(sizeof(int *) * height);
 
 	if (p == NULL)
 		return (NULL);
 
-	while (i < (height * width))
+
+	while (i < height)
 	{
-		p[i] = 0;
+		p[i] = (int) malloc(sizeof(int) * width);
+		
+		if (p[i] == NULL)
+		{
+			while(i >= 0)
+			{
+				free(p[i]);
+				i--;
+			}
+			return (NULL);
+		}
 		i++;
 	}
+
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+		{
+			p[i][j] = 0;
+		}
+
 
 	return (p);
 }
