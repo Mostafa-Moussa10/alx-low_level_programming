1701e640f951f2bed9a3722175b329394e43ddcb
mostafa <mostafabadry95@gmail.com>
2023-07-14 06:41:27 -0700

    memory allocation

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -18,11 +18,15 @@ char *create_array(unsigned int size, char c)
 	if (size == 0)
 		return (NULL);
 
-	p = (char*) malloc(sizeof(char) * size);
+	p = (char *) malloc(sizeof(char) * size);
+
+	if (p == NULL)
+		return (NULL);
 
 	for (i = 0; i < size; i++)
 	{
 		p[i] = c;
+
 	}
 
 	return (p);
