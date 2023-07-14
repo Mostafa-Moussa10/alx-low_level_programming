01970570fdac8fcc3291ada11bf2a74ea1932e92
mostafa <mostafabadry95@gmail.com>
2023-07-14 07:59:16 -0700

    memory allocation

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -16,7 +16,7 @@ char *str_concat(char *s1, char *s2)
 	unsigned int i = 0;
 	unsigned int j = 0;
 	char *p;
-	
+
 	if (s1 == NULL)
 		s1 = "";
 
@@ -34,8 +34,6 @@ char *str_concat(char *s1, char *s2)
 		p[i] = s1[i];
 		i++;
 	}
-
-	
 	while (s2[j] != '\0')
 	{
 		p[i] = s2[j];
