f73524fd2238178f444f0e92dfd8ea3974edb1d4
mostafa <mostafabadry95@gmail.com>
2023-07-17 13:00:11 -0700

    auto process

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <header.h>
+#include <stdio.h>
+/**
+ * main - function to print the name of current file
+ *
+ * Return: 1 if success
+ */
+
+int main(void)
+{
+	printf("%s", FILE);
+
+	return (1);
+}
