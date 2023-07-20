673c624bcdf285e028755b0254253a25d0133761
mostafa <mostafabadry95@gmail.com>
2023-07-20 12:09:40 -0700

    automan

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -11,4 +11,11 @@
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
+	int i = 0; /*counter*/
+
+	if (size == NULL)
+		return;
+	for (i = 0; i < size; i++)
+		action(array[i]);
+}
 
