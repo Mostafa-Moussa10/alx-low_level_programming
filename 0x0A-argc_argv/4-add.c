5b79ef0666d65a8d49fd04df9f049bd039cc7c19
mostafa <mostafabadry95@gmail.com>
2023-07-21 05:48:31 -0700

    automan

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - function to get the sum of numbers
+ * @argc: number of arguments
+ * @argv: string of arguments
+ * Return: 0 if success
+ */
+
+int main(int argc, char **argv)
+{
+
+	int i = 0;
+	int sum = 0;
+	
+	for (i = 1; i < argc; i++)
+	{
+
+		if (!(atoi(argv[i])))
+
+		{
+			printf("Error\n");
+			exit(1);
+		}
+		
+		
+		sum += atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+
+	return (0);
+}
