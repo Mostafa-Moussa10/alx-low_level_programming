6ff63f5495608162b7d683ac48d70da2e8acd806
mostafa <mostafabadry95@gmail.com>
2023-07-21 05:15:04 -0700

    automan

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - function to print number of arguments
+ * @argc: number of arguments
+ * @argv: string of arguments
+ *
+ * Return: 0 if success
+ */
+
+int main(int argc, char **argv)
+{
+	printf("%d\n", argc);
+	return (0);
+}
+
