02ac41c4fcc5e0132f3f23fae460f7a33ede43ab
mostafa <mostafabadry95@gmail.com>
2023-07-22 05:05:00 -0700

    automan

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - function to count the number of coins
+ *
+ * @argc: number of arguments
+ * @argv: string to the argumetns
+ * Return: always 1
+ */
+
+int main(int argc, char **argv)
+{
+
+	return (1);
+}
