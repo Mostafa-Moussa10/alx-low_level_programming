eea6e573158379f846c6b3804b1d295ee96f3ffb
mostafa <mostafabadry95@gmail.com>
2023-07-28 12:24:27 -0700

    automan

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * add_node - function to add node to the linked list
+ * @head: head of linked list
+ * @str: data of node
+ * Return: address of new element
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *s;
+
+	s = malloc(sizeof(list_t));
+	s->str = str;
+	s->next = head;
+	head = s;
+	return (s);
+}
