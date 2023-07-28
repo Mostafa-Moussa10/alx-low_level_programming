ef3f3380b70c8ba6f6d4e8e4a594f8163e30b170
mostafa <mostafabadry95@gmail.com>
2023-07-28 12:56:14 -0700

    automan

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -11,10 +11,16 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *s;
+	unsigned int len = strlen(str);
+
 	s = malloc(sizeof(list_t));
+	if (s == NULL)
+		return (NULL);
+
 	s->str = strdup(str);
+	s->len = len;
 	s->next = *head;
-	head = &s;
-	
+	*head = s;
+
 	return (s);
 }
