cb1e1d492f629ecd6c92db10af3f9f44f7a3ceed
mostafa <mostafabadry95@gmail.com>
2023-07-28 14:02:55 -0700

    automan

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,12 +10,12 @@ size_t print_list(const list_t *h)
 {
 	size_t count = 0;
 
-	while (h)
+	while (h != NULL)
 	{
-		if (h->str != NULL)
-			printf("[%u] %s\n", h->len, h->str);
-		else
+		if (h->str == NULL)
 			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
 		
 		h = h->next;
 		count++;
