713701657aa15592edfcd39c5a31ff165ac269d9
mostafa <mostafabadry95@gmail.com>
2023-07-28 13:08:05 -0700

    automan

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <string.h>
+
+/**
+ * free_list - function to free a linked list
+ * @head: pointer to linked list
+ */
+
+void free_list(list_t *head)
+{
+	list_t *s;
+
+	s = head;
+	while (!s->next)
+	{
+		free(s);
+		s = s->next;
+	}
+}
+
