f10d8636d851a9618b4f632f51e9c0e11568d966
mostafa <mostafabadry95@gmail.com>
2023-07-29 13:24:30 -0700

    auto

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - function to get the string length
+ *
+ * @s: pointer to string
+ * Return: string length
+ */
+
+int _strlen_recursion(char *s)
+{
+	int i = 0;
+	if (s[0])
+	{
+		i++;
+		i = i +_strlen_recursion(s+1);
+	}
+	return i;
+}
