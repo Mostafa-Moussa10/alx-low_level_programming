edb63fbfa094a66617106c5c0aba68ac54871d53
mostafa <mostafabadry95@gmail.com>
2023-07-31 13:35:45 -0700

    automan

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_listint - function to clear the linked list
+ * @head: poitner to header of linked list
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *s;
+
+	while (head)
+	{
+		s = head->next;
+		free(headd);
+		head = s;
+	}
+
+}
