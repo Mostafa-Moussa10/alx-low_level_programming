4bd6d66288be4dac0bc5c1dc923010fa25d9bd0e
mostafa <mostafabadry95@gmail.com>
2023-07-31 11:52:06 -0700

    automan

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,13 +1,13 @@
 #include "lists.h"
 
 /**
- * print_listint - function to print all element of a list
+ * listint_len - function to get size of a list
  * @h: header of the linked list
  * Return: the size of linked list (number of nodes)
  */
 
 
-size_t print_listint(const listint_t *h)
+size_t listint_len(const listint_t *h)
 {
 	size_t count = 0;
 	const listint_t *s;
