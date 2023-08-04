0e050b68b31ef583beb704bb1702ff5457adb8ea
mostafa <mostafabadry95@gmail.com>
2023-08-04 09:24:42 -0700

    automan

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -12,7 +12,7 @@ int get_bit(unsigned long int n, unsigned int index)
 	int bit = 0;
 	unsigned int i = 0;
 	unsigned long int x = n;
-	
+
 	while (n)
 	{
 		i++;
@@ -22,7 +22,7 @@ int get_bit(unsigned long int n, unsigned int index)
 		return (-1);
 
 	bit = (x >> index) & 1;
-	
+
 
 	return (bit);
 }
