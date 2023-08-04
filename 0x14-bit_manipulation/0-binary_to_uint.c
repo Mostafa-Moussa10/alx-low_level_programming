496fdb9134452fc21c0fdada9e0c6f4417e05e6f
mostafa <mostafabadry95@gmail.com>
2023-08-04 07:53:10 -0700

    automan

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -11,7 +11,7 @@ unsigned int binary_to_uint(const char *b)
 	unsigned int num = 0;
 	int i = 0;
 	int j = 0;
-	
+
 	if (b == NULL)
 		return (0);
 
@@ -19,15 +19,14 @@ unsigned int binary_to_uint(const char *b)
 	/*checker*/
 	while (b[i] != '\0')
 	{
-		if(b[i] != '0' && b[i] != '1')
+		if (b[i] != '0' && b[i] != '1')
 			return (0);
 		i++;
 	}
-	
 
 	while (b[i] != '\0')
 		i++;
-	
+
 	i--;
 
 	while (i + 1)
