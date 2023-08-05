0630dd66b3ce5329b24264951f42c6ad80cc8ddb
mostafa <mostafabadry95@gmail.com>
2023-08-05 10:53:16 -0700

    automan

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -2,7 +2,7 @@
 
 /**
  * set_bit - function to set bit to 1
- * @n: pointer to int 
+ * @n: pointer to int
  * @index: index of bit to change
  * Return: 1 if it worked, or -1 if an error occurred
  */
