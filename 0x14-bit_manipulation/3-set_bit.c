5130c6453847fec5b03892c0c68022e77aec4017
mostafa <mostafabadry95@gmail.com>
2023-08-05 10:52:39 -0700

    automan

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * set_bit - function to set bit to 1
+ * @n: pointer to int 
+ * @index: index of bit to change
+ * Return: 1 if it worked, or -1 if an error occurred
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	*n = *n | (1 << index);
+
+	return (1);
+}
