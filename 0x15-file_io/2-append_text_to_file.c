672178a1c49eb7f642fbd0015631b86afda4a853
mostafa <mostafabadry95@gmail.com>
2023-08-10 12:29:15 -0700

    automan

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -11,6 +11,7 @@
 int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd;
+	int w;
 
 	if (!filename)
 		return (-1);
@@ -19,7 +20,9 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 
-	write(fd, text_content, strlen(text_content));
+	w = write(fd, text_content, strlen(text_content));
+	if (w == -1)
+		return (-1);
 
 	close(fd);
 
