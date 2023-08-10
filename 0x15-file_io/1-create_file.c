9185093368b4f2592ed1a2f61cf5f259263a2f1e
mostafa <mostafabadry95@gmail.com>
2023-08-10 11:42:35 -0700

    automan

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * create_file - fuction to create file
+ * @filename: pointer to file name
+ * @text_content: content of text
+ * Return: 1 on success, -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+
+	if (!filename)
+		return (-1);
+
+	fd = open(filename, O_CREATE | O_TRUUNCAT | O_WRONLY);
+	if (fd == -1)
+		return (-1);
+	if (text_content)
+		write(fd, text_content, strlen(text_content));
+	close(fd);
+	
+	return (1);
+}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,13 +1,15 @@
 #ifndef MAIN_H_
 #define MAIN_H_
 
+#include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include <sys/types.h>
 #include <sys/stat.h>
-
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
 ssize_t read_textfile(const char *filename, size_t letters);
 int _putchar(char c);
 
