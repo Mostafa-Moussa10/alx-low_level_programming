41bf7bbd85e41acf4aa9795d470424997b9cead3
mostafa <mostafabadry95@gmail.com>
2023-08-11 10:56:24 -0700

    automan

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -9,44 +9,59 @@
 
 int main(int argc, char **argv)
 {
-	int fd;
+	int fd1;
+	int fd2;
 	int fc;
+	int r;
+	int w;
 	char *buf;
 
 	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
+	}
 
-	fd = open(argv[1], O_RDONLY);
-	if (fd == -1)
+	fd1 = open(argv[1], O_RDONLY);
+	if (fd1 == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from %s\n", argv[1]);
 		exit(98);
 	}
-	buf = malloc(sizeof(char) * 1024);
-	read(fd, buf, 1024);
-	fc = close(fd);
-	if (fc ==  -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd%d\n", fd);
-		exit(100);
-	}
 
-	fd = open(argv[2], O_WRONLY | O_TRUNC | O_CREAT, 0664);
-	if (fd == -1)
+	buf = malloc(sizeof(char) * 1024);
+	r = read(fd1, buf, 1024);
+	fd2 = open(argv[2], O_WRONLY | O_TRUNC | O_CREAT, 0664);
+	if (fd2 == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[1]);
-                exit(99);
+		exit(99);
 	}
 
-	write(fd, buf, 1024);
-	free(buf);
-	fc = close(fd);
-	if (fc ==  -1)
-        {
-                dprintf(STDERR_FILENO, "Error: Can't close fd%d\n", fd);
-                exit(100);
+	while (r > 0)
+	{
+		w = write(fd2, buf, r);
+		fd2 = open(argv[2], O_WRONLY | O_APPEND);
+		if (fd2 == -1 || w == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			exit(99);
+		}
+		r = read(fd1, buf, 1024);
+
 	}
-	printf("IT IS SUCCESS\n");
+
+	fc = close(fd1);
+	if (fc == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd%d\n", fd1);
+		exit(100);
+	}
+	fc = close(fd2);
+	if (fc == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd%d\n", fd1);
+		exit(100);
+	}
 	return (1);
 }
-
