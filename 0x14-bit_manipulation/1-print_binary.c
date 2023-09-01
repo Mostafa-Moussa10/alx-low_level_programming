e500e07c6da308b3753a0a3e6216e0e3a9a055a6
mostafa <mostafabadry95@gmail.com>
2023-09-01 05:32:55 -0700

    the auto man

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_binary - function to print number in binary form
+ *
+ * @n: number to print
+ */
+
+void print_binary(unsigned long int n)
+{
+	int i = 0;
+	int count= 0;
+	unsigned long int current;
+
+	if (n == 0)
+		_putchar('0');
+
+	for (i = 63; i >= 0; i--)
+	{
+		current = n >> i;
+		if (current & 1)
+		{
+			_putchar('1');
+			count++;
+		}
+		else if (count)
+			_putchar('0');
+	}
+
+}
+
