c7760b234bd74368196a29a228b7b6b14f6eac7a
mostafa <mostafabadry95@gmail.com>
2023-09-01 05:43:04 -0700

    the auto man

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * get_bit - function to get bit value 0 or 1
+ * @n: number to check its bit
+ * @index: bit number
+ * Return: the value of given bit
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (n & (1 << index))
+		return (1);
+	else
+		return (0);
+	
+}
+
