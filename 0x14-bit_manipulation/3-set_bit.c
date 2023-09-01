499cf5382914290ce1e15a7826b1400c89d7950b
mostafa <mostafabadry95@gmail.com>
2023-09-01 05:50:02 -0700

    the auto man

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * set_bit - function to set bit
+ * @n: pointer to integer
+ * @index: number of bit
+ * Return: 1 if it worked, or -1 if an erroe occurred
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > 63)
+		return (-1);
+
+	*n = *n | (1 << index);
+
+	return (1);
+}
+
