917606b992a155d1b365a341a09c8ca853bed1bd
mostafa <mostafabadry95@gmail.com>
2023-09-04 10:50:43 -0700

    automan

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * create_file - function to create file
+ * @filename: pointer to file name
+ * @content: content of file
+ * Return: 1 on success, -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+
+	if (filename == NULL)
+		return (0);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 600);
+	if (fd == -1)
+		return (0);
+	if (text_context != NULL)
+		w = write(fd, text_content, strlen(text_content));
+
+	
+	return (1);
+}
+
