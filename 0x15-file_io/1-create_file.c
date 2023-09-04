1a1bc48b9c230fb194da971c50a75ab51e8a5a19
mostafa <mostafabadry95@gmail.com>
2023-09-04 11:06:47 -0700

    automan

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,14 +10,14 @@
 int create_file(const char *filename, char *text_content)
 {
 	int fd;
-
+	
 	if (filename == NULL)
 		return (0);
 	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 600);
 	if (fd == -1)
 		return (0);
-	if (text_context != NULL)
-		w = write(fd, text_content, strlen(text_content));
+	if (text_content != NULL)
+		write(fd, text_content, strlen(text_content));
 
 	
 	return (1);
