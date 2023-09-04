6819013464869cb87e081fbbb39b5959d10b33d2
mostafa <mostafabadry95@gmail.com>
2023-09-04 10:01:09 -0700

    automan

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * read_textfile - function to read the content of the file
+ * @filename: pointer to file name as string
+ * @letters: number of letters to read
+ * Return: the number of read letters
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t count;
+	int fd;
+
+	if (filename == NULL)
+		return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	count = read(fd, STDOUT_FILENO, letters);
+
+	return (count);
+}
+
+
