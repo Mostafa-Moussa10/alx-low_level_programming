8b9fdb262b171215a55c2fb3e1581aa4ac078b7f
mostafa <mostafabadry95@gmail.com>
2023-09-04 10:13:50 -0700

    automan

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -11,13 +11,19 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 	ssize_t count;
 	int fd;
+	char *buf;
+
+	buf = malloc(sizeof(char) * letters);
+	if (buf == NULL)
+		return (0);
 
 	if (filename == NULL)
 		return (0);
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
-	count = read(fd, STDOUT_FILENO, letters);
+	count = read(fd, buf, letters);
+	write (STDOUT_FILENO, buf, count);
 
 	return (count);
 }
